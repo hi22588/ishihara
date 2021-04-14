88fade2339e0752e671201087a51b72885310ea8
hi22588 <82525683+hi22588@users.noreply.github.com>
2021-04-14 23:49:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,58 +14,27 @@ line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 
-@app.route("/", methods=["GET", "POST"])    
-def test(keyword):
-        flex_message = FlexSendMessage(
-            alt_text='hello',
-            contents={
-                "type": "bubble",
-                "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                    {
-                        "type": "text",
-                        "text": "hello, world"
-                    }
-                    ]
-                },
-                "footer": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                    {
-                        "type": "button",
-                        "action": {
-                        "type": "postback",
-                        "label": "action",
-                        "data": "測試hello{}".format(keyword),
-                        "displayText": "測試{}".format(keyword)
-                        }
-                    }
-                    ]
-                }
-            }
-        )    
-        return flex_message
+@app.route("/", methods=["GET", "POST"])
+def callback():
 
-@handler.add(MessageEvent, message=TextMessage)   
-import re 
-   if re.search(r'測試$', event.message.text.lower()) != None:
-        #我好肥 測試  
-        keyword = event.message.text[:-2] #我好肥
-        print(keyword)
-        abc = test(keyword) #call模板 並代入切片後的字串
-        line_bot_api.reply_message(event.reply_token, abc)
-        return 0
-    
-def handle_postback(event):
-    ts = event.postback.data
-    print(ts)
-    keyword = ts[7:]
-    print(keyword)
-    if ts[7:] == '{}'.format(keyword):
+    if request.method == "GET":
+        return "Hello Heroku"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
 
-        text_message = TextSendMessage(text='訊息{}'.format(keyword))
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
 
-        line_bot_api.reply_message(event.reply_token, text_message)    
+        return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    get_message = event.message.text
+
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
